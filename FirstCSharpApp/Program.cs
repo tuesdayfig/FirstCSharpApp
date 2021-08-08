49f27a2e1e6670fe5788985d63a5887b3701f171
dontaskmeaboutit@gmail.com <dontaskmeaboutit@gmail.com>
2021-08-08 12:14:15 -0400

    Add project files.

---
+++ FirstCSharpApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int randomInt = 1000;
+            double doubleThis = 0.1;
+            string stringText = "Hello World";
+            bool tOrF = true;
+            char randomChar = "a";*/
+
+            int myInt;
+            myInt = 5;            
+        }
+    }
+}
